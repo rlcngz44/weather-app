96ebba2fc8d04edde64036654fa5b5c4b7581210
rlcngz <erolcengiz@gmail.com>
2022-08-13 13:42:03 +0200

    Print a small forecast to user

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-const request = require ("request")
+const request = require("request");
 
-const url = "http://api.weatherstack.com/current?access_key=046e2a9cebd90c1a6bfdd729db82ff33&query=amsterdam"
+const url =
+  "http://api.weatherstack.com/current?access_key=046e2a9cebd90c1a6bfdd729db82ff33&query=amsterdam";
 
-request({url:url, json:true}, (error, response)=>{
-    console.log(response.body.current)
-})
+request({ url: url, json: true }, (error, response) => {
+  //   console.log(response.body.current);
+  const temp = response.body.current.temperature;
+  const precip = response.body.current.precip;
+  console.log(
+    "It is currently " +
+      temp +
+      "degrees out. There is " +
+      precip +
+      "% chance of rain."
+  );
+});
+
+//Challenge! Print a small forecast to the user.\
+//1. Print "It is currently 29 degrees out. There is 0% chance of rain"
