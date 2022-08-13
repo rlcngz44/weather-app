6c6dea9ea0653607a061a710d81830e4cbedb993
rlcngz <erol.cengiz@appythings.com>
2022-08-13 13:18:43 +0200

    Update request with json parsing

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const request = require ("request")
 
 const url = "http://api.weatherstack.com/current?access_key=046e2a9cebd90c1a6bfdd729db82ff33&query=amsterdam"
 
-request({url:url}, (error, response)=>{
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({url:url, json:true}, (error, response)=>{
+    console.log(response.body.current)
 })
