8a979fdbb9217fa63dbaf4c8999a72cc687e7ffb
rlcngz <erolcengiz@gmail.com>
2022-08-15 09:26:41 +0200

    Update forecasting app

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const request = require("request");
 
 const url =
-  "http://api.weatherstack.com/current?access_key=046e2a9cebd90c1a6bfdd729db82ff33&query=amsterdam";
+  "http://api.weatherstack.com/current?access_key=046e2a9cebd90c1a6bfdd729db82ff33&query=amsterdam&units=m";
 
 request({ url: url, json: true }, (error, response) => {
   //   console.log(response.body.current);
   const temp = response.body.current.temperature;
-  const precip = response.body.current.precip;
+  const feelsLike = response.body.current.feelslike;
   console.log(
-    "It is currently " +
+    response.body.current.weather_descriptions[0] +
+      ". It is currently " +
       temp +
-      "degrees out. There is " +
-      precip +
-      "% chance of rain."
+      " degrees out. It feels like " +
+      feelsLike +
+      " degrees."
   );
 });
 
